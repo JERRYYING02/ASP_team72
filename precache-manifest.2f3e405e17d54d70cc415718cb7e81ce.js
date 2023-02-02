f333bbeddac90e8f80f78fc9f654d077543d8170
JERRYYING02 <90186306+JERRYYING02@users.noreply.github.com>
2023-02-03 03:43:56 +0800

    Updates

---
+++ precache-manifest.2f3e405e17d54d70cc415718cb7e81ce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c5e05bbdf8d8589a179",
+    "url": "/ASP_team72/static/css/main.340f2356.chunk.css"
+  },
+  {
+    "revision": "1c5e05bbdf8d8589a179",
+    "url": "/ASP_team72/static/js/main.1c5e05bb.chunk.js"
+  },
+  {
+    "revision": "e72e18cde8be384a4da2",
+    "url": "/ASP_team72/static/js/1.e72e18cd.chunk.js"
+  },
+  {
+    "revision": "6ad6386248eb0a51510a",
+    "url": "/ASP_team72/static/css/2.ef9f508d.chunk.css"
+  },
+  {
+    "revision": "6ad6386248eb0a51510a",
+    "url": "/ASP_team72/static/js/2.6ad63862.chunk.js"
+  },
+  {
+    "revision": "0fc4294ec11012895c97",
+    "url": "/ASP_team72/static/js/runtime~main.0fc4294e.js"
+  },
+  {
+    "revision": "1f023b349af1d79a72740f4cc881a310",
+    "url": "/ASP_team72/static/media/Montserrat-Bold.1f023b34.ttf"
+  },
+  {
+    "revision": "14d00dab1f6802e787183ecab5cce85e",
+    "url": "/ASP_team72/static/media/Poppins-Black.14d00dab.ttf"
+  },
+  {
+    "revision": "d532c6952933556064aeb608bdaa1053",
+    "url": "/ASP_team72/static/media/CantataOne-Regular.d532c695.ttf"
+  },
+  {
+    "revision": "77b06cd11f3cdbaa323ba1558c65683a",
+    "url": "/ASP_team72/index.html"
+  }
+];
